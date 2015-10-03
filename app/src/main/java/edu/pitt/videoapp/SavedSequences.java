73174bd469cefab0f99d7a2b7924c81413feff9c
rcon1012 <rcon1012@gmail.com>
2015-10-03 15:32:59 -0400

    create saved sequences page

--- app/src/main/java/edu/pitt/videoapp/SavedSequences.java
+++ app/src/main/java/edu/pitt/videoapp/SavedSequences.java
@@ -1,24 +1,22 @@
 package edu.pitt.videoapp;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SavedSequences extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_saved_sequences);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_saved_sequences, menu);
         return true;
     }
 
@@ -35,11 +33,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void toSelectSequence(View view)
-    {
-        Intent intent = new Intent(this, SavedSequences.class);
-        startActivity(intent);
     }
 }
