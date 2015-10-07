82f8c2f18e9b11fb66b782e7fb9b258cf882e0c0
cjp51 <cjp51@pitt.edu>
2015-10-07 18:57:39 -0400

    Added UI for other pages
    I updated the ui for the other activities.  Can start on logic now.

--- app/src/main/java/edu/pitt/videoapp/XMLActivity.java
+++ app/src/main/java/edu/pitt/videoapp/XMLActivity.java
@@ -12,22 +12,4 @@ public class XMLActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xml);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_xml, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-
-        return super.onOptionsItemSelected(item);
-    }
 }
