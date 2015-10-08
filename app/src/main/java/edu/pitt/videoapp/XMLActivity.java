0b7a5075d670eae23f33a9d22df7f1350af7eabd
rcon1012 <rcon1012@gmail.com>
2015-10-08 18:52:04 -0400

    create email dialog

--- app/src/main/java/edu/pitt/videoapp/XMLActivity.java
+++ app/src/main/java/edu/pitt/videoapp/XMLActivity.java
@@ -1,9 +1,10 @@
 package edu.pitt.videoapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class XMLActivity extends AppCompatActivity {
 
@@ -11,5 +12,15 @@ public class XMLActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xml);
+    }
+
+    public void showEmailDialog(View view)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(XMLActivity.this);
+        LayoutInflater inflater = XMLActivity.this.getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.email_dialog, null));
+        builder.create();
+        builder.show();
+
     }
 }
