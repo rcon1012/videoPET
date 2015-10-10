9519bffb94d3a35d907dce2144c15dbe7ce3f2cc
rcon1012 <rcon1012@gmail.com>
2015-10-10 18:06:47 -0400

    list view skeleton with mock data

--- app/src/main/java/edu/pitt/videoapp/XMLActivity.java
+++ app/src/main/java/edu/pitt/videoapp/XMLActivity.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class XMLActivity extends AppCompatActivity {
@@ -16,6 +18,15 @@ public class XMLActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xml);
+
+        // xml list view skeleton
+        // Get a handle to the list view
+        ListView lv = (ListView) findViewById(R.id.XMLlistView);
+
+        // mock data
+        String[] lv_arr = {"test1", "test2", "test3"};
+        lv.setAdapter(new ArrayAdapter<String>(XMLActivity.this,
+                android.R.layout.simple_list_item_1, lv_arr));
     }
 
     // shows the email dialog to email the xml
@@ -42,11 +53,12 @@ public class XMLActivity extends AppCompatActivity {
                 i.putExtra(Intent.EXTRA_EMAIL  , emails);
                 i.putExtra(Intent.EXTRA_SUBJECT, subject);
                 i.putExtra(Intent.EXTRA_TEXT   , body);
+                // to attach files to email
                 //i.putExtra(Intent.EXTRA_STREAM, Uri.parse("file:///sdcard/MyFile.csv"));
                 try {
                     startActivity(Intent.createChooser(i, "Send"));
                 } catch (android.content.ActivityNotFoundException ex) {
-                    Toast.makeText(XMLActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(XMLActivity.this, "No email clients installed", Toast.LENGTH_SHORT).show();
                 }
                 emailDialog.dismiss();
             }
