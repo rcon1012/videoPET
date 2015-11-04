9c3648d42f8d3319bc7575039bb727bd60365609
rcon1012 <rcon1012@gmail.com>
2015-11-03 19:00:59 -0500

    create activity test suite for Main Activity

---
+++ app/src/androidTest/java/edu/pitt/videoapp/MainActivityTest.java
@@ -0,0 +1,22 @@
+package edu.pitt.videoapp;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by Ryan on 11/3/2015.
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+        public MainActivityTest() {
+            super(MainActivity.class);
+        }
+
+        @Override
+        protected void setUp() throws Exception {
+            super.setUp();
+        }
+
+        @Override
+        protected void tearDown() throws Exception {
+            super.tearDown();
+        }
+}
