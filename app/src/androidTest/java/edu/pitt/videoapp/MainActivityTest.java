69b3b1c40eb403a75f991ef1c72b1518b451d3a4
rcon1012 <rcon1012@gmail.com>
2015-11-03 22:53:20 -0500

    tests for main page

--- app/src/androidTest/java/edu/pitt/videoapp/MainActivityTest.java
+++ app/src/androidTest/java/edu/pitt/videoapp/MainActivityTest.java
@@ -1,22 +1,40 @@
 package edu.pitt.videoapp;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.ViewAsserts;
+import android.test.suitebuilder.annotation.MediumTest;
 
 /**
  * Created by Ryan on 11/3/2015.
  */
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
-        public MainActivityTest() {
-            super(MainActivity.class);
-        }
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
 
-        @Override
-        protected void setUp() throws Exception {
-            super.setUp();
-        }
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
 
-        @Override
-        protected void tearDown() throws Exception {
-            super.tearDown();
-        }
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @MediumTest
+     public void testNewProjectButton() {
+        ViewAsserts.assertOnScreen(getActivity().getWindow().getDecorView(), getActivity().findViewById(R.id.main_button_1));
+    }
+
+    @MediumTest
+    public void testSetupsButton() {
+        ViewAsserts.assertOnScreen(getActivity().getWindow().getDecorView(), getActivity().findViewById(R.id.main_button_2));
+    }
+
+    @MediumTest
+    public void testXMLButton() {
+        ViewAsserts.assertOnScreen(getActivity().getWindow().getDecorView(), getActivity().findViewById(R.id.main_button_3));
+    }
+
 }
