de3a251f2f2b2b358bd7f8f61720f30260bbdab4
rcon1012 <rcon1012@gmail.com>
2015-11-22 19:31:11 -0500

    style email dialog and more small changes

--- app/src/main/java/edu/pitt/videoapp/MainActivity.java
+++ app/src/main/java/edu/pitt/videoapp/MainActivity.java
@@ -15,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.video_pet);
         setContentView(R.layout.activity_main);
     }
 
